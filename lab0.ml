6401ba4afe17577c4497c7ccca95af32cd699428
Jessica Choi <choi.jess816@gmail.com>
2017-09-19 02:53:25 -0700

    Add files via upload

---
+++ lab0.ml
@@ -0,0 +1,9 @@
+(* QUESTION 1 *)
+(* 4.8.0-34-generic *)
+(* /usr/bin/gcc *)
+(* /home/student/.opam/4.04.0/bin/ocaml *)
+
+
+(* QUESTION 2 *)
+(* val f : int -> int = <fun> *)
+(* val g : float -> float -> float = <fun> *)
---
+++ test.ml
@@ -0,0 +1,3 @@
+let f x = 3 * x * x + 4 * x + 5
+
+let g x y = x *. x +. y *. y
